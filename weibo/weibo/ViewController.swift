a92242c3a142b5aadfd3eaf408b0009d9dd36119
sharui <sr_sharui@qq.com>
2015-10-07 23:44:48 +0800

    ceshi

--- weibo/weibo/ViewController.swift
+++ weibo/weibo/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("1")
     }
 
     override func didReceiveMemoryWarning() {
